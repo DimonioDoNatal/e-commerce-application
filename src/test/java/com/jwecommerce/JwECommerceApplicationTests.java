6c3f7838586924b5827af0a679e5b178b3ec2f78
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2021-02-26 19:28:32 -0300

    first commit

---
+++ src/test/java/com/jwecommerce/JwECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
